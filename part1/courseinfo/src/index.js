1823ab6048cdf64896df01147571c4e0f6a272d3
Rafael <rafandpufff@gmail.com>
2023-08-22 13:52:18 +0200

    Añadir solución ex 1.8

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,6 +5,26 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({good, neutral, bad}) => {
+  // data for statistics
+  const clicks = good + neutral + bad;
+  const positiveClicks = clicks === 0 ? 0 : (good / clicks) * 100;
+  const averageClicks =
+    clicks === 0
+      ? 0
+      : (good * 1 + neutral * 0 + bad * -1) / clicks;
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {clicks}</p>
+      <p>average {averageClicks}</p>
+      <p>positive {positiveClicks} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -14,11 +34,6 @@ const App = () => {
   const handleClickGood = () => setGood(good + 1);
   const handleClickNeutral = () => setNeutral(neutral + 1);
   const handleClickBad = () => setBad(bad + 1);
-  // data for statistics
-  const clicks = good + neutral + bad;
-  const positiveClicks = clicks === 0 ? 0 : (good / clicks)*100;
-  const averageClicks =
-    clicks === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / clicks;
   return (
     <div>
       <h1>give feedback</h1>
@@ -26,12 +41,7 @@ const App = () => {
       <Button onClick={handleClickNeutral} text={"neutral"} />
       <Button onClick={handleClickBad} text={"bad"} />
       <h1>statistic</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {clicks}</p>
-      <p>average {averageClicks}</p>
-      <p>positive {positiveClicks} %</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
   );
 };
